cdbb6c127f6719782fa72e76976eac100fc6d5de
ehigie aito (Pystar) <aitoehigie@gmail.com>
2019-10-25 02:33:53 +0100

    add a setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from disutils import setup, find_packages
+
+setup(name="imager",
+version="1.0",
+description="Imager: a HDD warranty and MTBF information extractor OCR script.",
+author = "Ehigie Aito",
+author_email = "aitoehigie@gmail.com",
+packages = ["task1"],
+scripts=["imager.py"],
+)
+
